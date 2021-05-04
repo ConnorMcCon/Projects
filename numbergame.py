cc35eab8b26bb3671df6c0155a44e9d9f2481f6d
NameAintConnor <83537812+NameAintConnor@users.noreply.github.com>
2021-05-04 16:55:23 +0100

    Add files via upload

---
+++ numbergame.py
@@ -0,0 +1,27 @@
+#Connor McConaghy
+#Number game
+
+import random
+
+numberlist = range(1,100)
+randomnumber=random.choice(numberlist)
+
+
+def NumberSolution():
+    while True:
+        guess = int(input("Guess a number between 1-100 - "))
+        if guess < randomnumber:
+            print("Number is higher")
+        else:
+            print("Number is less")
+        if guess == randomnumber:
+            print("You guessed correctly")
+            break
+
+NumberSolution()
+
+
+    
+
+
+
